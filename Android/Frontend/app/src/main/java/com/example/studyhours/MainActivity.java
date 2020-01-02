e5dcada231792b6e5eb7f2d6b57c03f520abcc26
HTKshimo <shuangl@iastate.edu>
2020-01-02 02:21:07 -0600

    added chronometer feature to user default screen; added firebase to app; main activity temporarily set to login screen

--- Android/Frontend/app/src/main/java/com/example/studyhours/MainActivity.java
+++ Android/Frontend/app/src/main/java/com/example/studyhours/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.studyhours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton recordButton = findViewById(R.id.recordHoursButton);
+        final Chronometer chronometer = findViewById(R.id.chronometer);
+
+        recordButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    chronometer.start();
+                } else {
+                    // The toggle is disabled
+                    chronometer.stop();
+                    //TODO: popup ask if would like to submit
+                }
+            }
+        });
     }
 }
