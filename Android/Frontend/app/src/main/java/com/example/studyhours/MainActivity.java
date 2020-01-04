7a0de0b272ae90b40f480d1e8786778fe615009b
HTKshimo <shuangl@iastate.edu>
2020-01-04 00:31:35 -0600

    completed navigation bar functionality. Fixed toolbar by adding it to only app_bar_navigation.xml

--- Android/Frontend/app/src/main/java/com/example/studyhours/MainActivity.java
+++ Android/Frontend/app/src/main/java/com/example/studyhours/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.view.View;
 import android.widget.Chronometer;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
--- Android/Frontend/app/src/main/java/com/example/studyhours/NavigationActivity.java
+++ Android/Frontend/app/src/main/java/com/example/studyhours/NavigationActivity.java
@@ -2,11 +2,16 @@ package com.example.studyhours;
 
 import android.os.Bundle;
 
+import com.example.studyhours.ui.gallery.GalleryFragment;
+import com.example.studyhours.ui.home.HomeFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,7 +27,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.widget.TextView;
 
-public class NavigationActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private TextView navName;
@@ -31,32 +36,32 @@ public class NavigationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
 
+        setContentView(R.layout.nav_header_navigation);
         navName = findViewById(R.id.nav_name);
         navEmail = findViewById(R.id.nav_email);
-//        navName.setText(getString(R.string.nav_header_title,
-//                "Sister name"));
-//        navEmail.setText(getString(R.string.nav_header_subtitle,
-//                "SisterEmail@todo.com"));
+        navName.setText(getString(R.string.nav_header_title,
+                "Sister name"));
+        navEmail.setText(getString(R.string.nav_header_subtitle,
+                "SisterEmail@todo.com"));
 
+        setContentView(R.layout.activity_navigation);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.recordHoursScreen, R.id.hoursHistoryScreen)
+                R.id.nav_record, R.id.nav_history, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -76,5 +81,25 @@ public class NavigationActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (id == R.id.nav_record){
+            fragmentManager.beginTransaction()
+                    .replace(R.id.nav_host_fragment, new HomeFragment())
+                    .commit();
+        }else if (id == R.id.nav_history){
+            fragmentManager.beginTransaction()
+                    .replace(R.id.nav_host_fragment, new GalleryFragment())
+                    .commit();
+        }else if (id == R.id.nav_logout){
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
